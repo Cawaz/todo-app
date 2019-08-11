60c62ae77513dc61bd673da08b7823b3e4df45f1
Cawaz <3cawaz3@gmail.com>
2019-08-11 10:46:38 +0900

    -mタスクの追加・削除を実装

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -6,13 +6,7 @@ export class TodoApp extends React.Component {
   constructor(props) {
       super(props);
       this.state = {
-        todos: [
-          {id:0, title: "title0"},
-          {id:1, title: "title1"},
-          {id:2, title: "title2"},
-          {id:3, title: "title3"},
-          {id:4, title: "title4"},
-        ],
+        todos: [],
         nextId: 0
       };
     }
@@ -21,9 +15,24 @@ export class TodoApp extends React.Component {
     return (
       <div>
         <h2>TodoApp</h2>
-          <AddTodo />
-          <List todos={this.state.todos} />
+          <AddTodo addTodo={ this.addTodo }  />
+          <List deleteTodo={ this.deleteTodo } todos={ this.state.todos } />
       </div>
     );
   }
+
+  addTodo = title => {
+    this.setState({
+      todos: [...this.state.todos, { id: this.state.nextId + 1, title: title }],
+      nextId: this.state.nextId + 1
+   });
+  };
+
+  deleteTodo = id => {
+    this.setState({
+      todos: this.state.todos.filter(todo => {
+        return todo.id !== id;
+      })
+    });
+  };
 }
